b2713a39b1b41f4b843394daa1616ce0a12ab3b9
juansesu3 <juanseu3@hotmail.com>
2023-07-31 11:06:43 +0200

    FEATURE(arquitecture)

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+
+const DashboardPage = () => {
   return (
     <>
-     
       <Button>Button</Button>
-
     </>
   );
-}
+};
+
+export default DashboardPage;
---
+++ ai-saas/app/(landing)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const LandingPage = (props: Props) => {
+    return (
+        <div>
+            <h1>Landing page (Unprotected)</h1>
+        </div>
+    )
+}
+
+export default LandingPage; 
