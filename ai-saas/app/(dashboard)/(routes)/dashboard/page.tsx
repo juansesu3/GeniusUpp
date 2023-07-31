32dbe7bef9595eb9ec244813d33713f52fc5491d
juansesu3 <juanseu3@hotmail.com>
2023-07-31 14:59:04 +0200

    FEATURE(arquitecture) i'm still working on arquitecture

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 const DashboardPage = () => {
   return (
     <>
-      <Button>Button</Button>
+     <p>Dashborad Page (Protected)</p>
     </>
   );
 };
