0996ddbb145ee60bea861c973fe2f8e522a7b480
juansesu3 <juanseu3@hotmail.com>
2023-08-02 11:45:35 +0200

    FEATURE(Sidebar) working on arquitecture and routes of side bar

--- ai-saas/components/Navbar.tsx
+++ ai-saas/components/Navbar.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidevbar from "./MobileSidebar";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidevbar />
       <div className="flex w-full justify-end ">
         <UserButton afterSignOutUrl="/" />
       </div>
--- ai-saas/components/Sidebar.tsx
+++ ai-saas/components/Sidebar.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import {
@@ -9,7 +10,7 @@ import {
   MessageSquare,
   ImageIcon,
   VideoIcon,
-  MusicIcon, 
+  MusicIcon,
   Code,
   Settings,
 } from "lucide-react";
@@ -61,11 +62,11 @@ const routes = [
     label: "Settings",
     icon: Settings,
     href: "/settings",
-  
   },
 ];
 
 const Sidebar = (props: Props) => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -80,7 +81,12 @@ const Sidebar = (props: Props) => {
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
               key={route.href}
               href={route.href}
             >
